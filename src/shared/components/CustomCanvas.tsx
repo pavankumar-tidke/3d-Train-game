f9076d1e90fdfd631d44605e88e2f47e62338047
pavankumar-tidke <pavankumartidke12345@gmail.com>
2024-04-21 22:27:47 +0530

    svg addded

--- src/shared/components/CustomCanvas.tsx
+++ src/shared/components/CustomCanvas.tsx
@@ -1,37 +1,33 @@
-import React, { useEffect, useRef } from 'react';
-import { Track } from '../utils/Track';
+import React, { useRef, useEffect } from 'react';
+import { ASSETS } from '../constants/constants';
 
-const CustomCanvas: React.FC = () => {
+const CustomCanvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    if (!canvas) return;
+    const context = canvas?.getContext('2d');
 
-    const ctx = canvas.getContext('2d');
-    if (!ctx) return;
+    if (!canvas || !context) return;
 
-    // Define track coordinates
-    const track1 = new Track(50, 350, 150, 250);
-    // const track2 = new Track(50, 150, 150, 150);
+    const image = new Image();
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(image, 10, 10);
+    };
 
-    // Clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    image.src = ASSETS.TRIAL_SVG; // Use the imported SVG file as the source
 
-    // Draw tracks on the canvas
-    track1.draw(ctx);
-    // track2.draw(ctx);
   }, []);
 
   return (
     <canvas
       ref={canvasRef}
-      className={`border-2 border-green-400 w-full`}
-      width={400} // Set canvas width
-      height={400} // Set canvas height
+      className="w-full h-full border-2 border-blue-400"
     ></canvas>
   );
-}
+};
 
 export default CustomCanvas;
---
+++ src/shared/constants/constants.ts
@@ -0,0 +1,8 @@
+
+
+// images and svg elements
+import TRIAL_SVG from '../assets/trial.svg';
+
+export const ASSETS = {
+    TRIAL_SVG: TRIAL_SVG,
+}
